9716512ff7e10621ceb316e6a769b89bb3325b49
dmh1001 <dmh1001@alu.ubu.es>
2021-09-03 17:02:16 +0200

    #73 nuevas métricas de medición

--- MonitorizacionIOT_1.0_all/usr/bin/Monitorizacion-IOT/Prediccion/Realizar_testeo.py
+++ MonitorizacionIOT_1.0_all/usr/bin/Monitorizacion-IOT/Prediccion/Realizar_testeo.py
@@ -1,13 +1,22 @@
 import sys
 from Testeo import *
 
+PATH="/usr/bin/Prediccion"
+
 if __name__ == "__main__":
 
     idSensor = sys.argv[1]
     fecha_inicio = sys.argv[2]
     fecha_final = sys.argv[3]
 
-    rmse = Testeo.prediccion_RMSE(idSensor, fecha_inicio, fecha_final)
+    print(fecha_inicio)
 
-    with open(PATH + '/testeo.txt', "a") as file:
-        file.write(str(fecha_inicio) + " - " + str(fecha_final) + " : " + str(rmse) )
+    tester = Testeo(idSensor)
+
+    rmse = tester.metrica_RMSE(fecha_inicio, fecha_final)
+    mae = tester.metrica_MAE(fecha_inicio, fecha_final)
+    mse = tester.metrica_MSE(fecha_inicio, fecha_final)
+    r2 = tester.metrica_R2(fecha_inicio, fecha_final)
+
+    with open(PATH + '/ResultadosTest/testeo.txt', "a") as file:
+        file.write(str(fecha_inicio) + " - " + str(fecha_final) + " RMSE: " + str(rmse) + " MAR: " + str(mae) + " MSE: " + str(mse) + " R2: " + str(r2) )
--- MonitorizacionIOT_1.0_all/usr/bin/Monitorizacion-IOT/Prediccion/Testeo.py
+++ MonitorizacionIOT_1.0_all/usr/bin/Monitorizacion-IOT/Prediccion/Testeo.py
@@ -6,34 +6,65 @@ from Campos import *
 from Extractor import *
 
 from math import sqrt
+
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
 
 PATH="/usr/bin/Prediccion"
 
 class Testeo:
-    def prediccion_RMSE(idSensor, fecha_inicio, fecha_final):
-        real = Extractor.extraer_data(idSensor,fecha_inicio, fecha_final ,Campos.VALOR)
-        pred = Extractor.extraer_data(idSensor,fecha_inicio, fecha_final ,Campos.PREDICCION)
 
+    def __init__(self, idSensor):
+        self.idSensor = idSensor
+
+
+    def _obtener_valores(self,fecha_inicio, fecha_final):
+
+        realVals = []
+        predictVals = []
+
+        real = Extractor.extraer_data(self.idSensor,fecha_inicio, fecha_final ,Campos.VALOR)
+        pred = Extractor.extraer_data(self.idSensor,fecha_inicio, fecha_final ,Campos.PREDICCION)
 
         time = datetime.strptime(fecha_inicio, '%d/%m/%Y %H:%M:%S')
         timeEnd = datetime.strptime(fecha_final, '%d/%m/%Y %H:%M:%S')
-
-        realVals = []
-        predinctVals = []
 
         while time <= timeEnd:
             try:
                 assert(pred[time])
                 assert(real[time])
 
-                predinctVals.append(pred[time])
+                predictVals.append(pred[time])
                 realVals.append(real[time])
+
             except:
                 pass
 
             time = time + relativedelta(minutes=1)
 
-        rmse = sqrt(mean_squared_error(realVals, predinctVals))
+        return realVals, predictVals
 
-        return rmse
+    def metrica_RMSE(self, fecha_inicio, fecha_final):
+
+        realVals, predictVals = self._obtener_valores(fecha_inicio, fecha_final)
+        return sqrt(mean_squared_error(realVals, predictVals))
+
+
+    def metrica_MAE(self, fecha_inicio, fecha_final):
+
+        realVals, predictVals = self._obtener_valores(fecha_inicio, fecha_final)
+        return mean_absolute_error(realVals, predictVals)
+
+
+    def metrica_MSE(self, fecha_inicio, fecha_final):
+
+        realVals, predictVals = self._obtener_valores(fecha_inicio, fecha_final)
+        return mean_squared_error(realVals, predictVals)
+
+
+    def metrica_R2(self, fecha_inicio, fecha_final):
+
+        realVals, predictVals = self._obtener_valores(fecha_inicio, fecha_final)
+        return r2_score(realVals, predictVals)
+
